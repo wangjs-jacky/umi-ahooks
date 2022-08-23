74a5c96335f2093d90558b145cdcab5303badd0e
所无聊 <2409277719@qq.com>
2022-08-24 07:17:46 +0800

    更新路由目录

--- .umirc.ts
+++ .umirc.ts
@@ -25,42 +25,44 @@ export default defineConfig({
     {
       name: 'AHooks-首页',
       path: '/ahooks',
-      component: './AHooks',
-    },
-    {
-      name: 'useLatest',
-      path: '/ahooks/0',
-      component: './AHooks/0',
-    },
-    {
-      name: 'useRef',
-      path: '/ahooks/1',
-      component: './AHooks/1.tsx',
-    },
-    {
-      name: 'useCreation',
-      path: '/ahooks/2',
-      component: './AHooks/2',
-    },
-    {
-      name: 'useUpdate',
-      path: '/ahooks/3',
-      component: './AHooks/3',
-    },
-    {
-      name: 'useReactive',
-      path: '/ahooks/4',
-      component: './AHooks/4',
-    },
-    {
-      name: 'useHover',
-      path: '/ahooks/5',
-      component: './AHooks/5',
-    },
-    {
-      name: '时间相关钩子',
-      path: '/ahooks/6',
-      component: './AHooks/6',
+      exact: true,
+      routes: [
+        {
+          name: 'useLatest',
+          path: '/ahooks/0',
+          component: './AHooks/0',
+        },
+        {
+          name: 'useRef',
+          path: '/ahooks/1',
+          component: './AHooks/1.tsx',
+        },
+        {
+          name: 'useCreation',
+          path: '/ahooks/2',
+          component: './AHooks/2',
+        },
+        {
+          name: 'useUpdate',
+          path: '/ahooks/3',
+          component: './AHooks/3',
+        },
+        {
+          name: 'useReactive',
+          path: '/ahooks/4',
+          component: './AHooks/4',
+        },
+        {
+          name: 'useHover',
+          path: '/ahooks/5',
+          component: './AHooks/5',
+        },
+        {
+          name: '时间相关钩子',
+          path: '/ahooks/6',
+          component: './AHooks/6',
+        },
+      ]
     },
   ],
   npmClient: 'yarn',
--- src/components/useRef.tsx
+++ src/components/useRef.tsx
@@ -3,7 +3,6 @@ import { useRef } from "react";
 const useLatest = (value: any) => {
     const ref = useRef(value);
     ref.current = value;
-
     return ref;
 }
 
